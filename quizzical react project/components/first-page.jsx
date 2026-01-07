1312132b8a8c028a5f2f2568fe7abddbfe25b0a2
Krish Shah <krishcshah09@gmail.com>
2026-01-07 20:54:42 +0530

    added first page components

---
+++ quizzical react project/components/first-page.jsx
@@ -0,0 +1,9 @@
+export default function(){
+    return(
+     <>
+        <h1>Quizzical</h1>
+        <p>Try solving this quiz</p>
+        <button>Start Quiz</button>
+    </>
+    )
+}
--- quizzical react project/src/App.jsx
+++ quizzical react project/src/App.jsx
@@ -1,9 +1,8 @@
+import FirstPage from '../components/first-page.jsx'
 export default function(){
   return(
     <>
-    <h1>Quizzical</h1>
-    <p>Try solving this quiz</p>
-    <button>Start Quiz</button>
+      <FirstPage/>
     </>
   )
 }
