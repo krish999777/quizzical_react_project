7d047717b3713b20671542655da9c7570ab81523
Krish Shah <krishcshah09@gmail.com>
2026-01-10 10:42:18 +0530

    added feature for picking option and added styling for the same

--- quizzical react project/components/quiz-elements.jsx
+++ quizzical react project/components/quiz-elements.jsx
@@ -2,16 +2,23 @@ import {decode} from 'html-entities'
 import {useState} from 'react'
 export default function(props){
     const [options]=useState(()=>generateOptions())
+    const [clickedOption,setClickedOption]=useState(0)
     function generateOptions(){
+        let incorrectAnswers=props.incorrectAnswers
         let options=["","","",""]
         options[Math.floor((Math.random()*4))]=props.correctAnswer
         options.forEach((each,index)=>{
             if(each===""){
-                options[index]=props.incorrectAnswers.pop()
+                options[index]=incorrectAnswers.pop()
             }
         })
         return options
     }
+    function toggleClickedOption(event){
+        setClickedOption(event.target.id)
+    }
+
+
     return(
         <>
             <div className="quiz-elements">
@@ -19,7 +26,7 @@ export default function(props){
             <div className="quiz-buttons-container">
                 {options.map((option,index)=>{
                     return(
-                        <button key={index} className="quiz-options">{decode(option)}</button>
+                        <button key={index} id={index+1} onClick={target=>toggleClickedOption(target)} className={`quiz-options ${clickedOption==index+1?'clicked':""}`}>{decode(option)}</button>
                     )
                 })}
             </div>
