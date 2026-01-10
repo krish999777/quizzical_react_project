3106c9d76e06e828b7fe896e2447de9f83b4fe44
Krish Shah <krishcshah09@gmail.com>
2026-01-10 09:48:54 +0530

    fixed issues with options making it on state

--- quizzical react project/components/quiz-elements.jsx
+++ quizzical react project/components/quiz-elements.jsx
@@ -1,21 +1,27 @@
 import {decode} from 'html-entities'
+import {useState} from 'react'
 export default function(props){
-    const options=["","","",""]
-    options[Math.floor((Math.random()*4))]=props.correctAnswer
-    options.forEach((each,index)=>{
-        if(each===""){
-            options[index]=props.incorrectAnswers.pop()
-        }
-    })
+    const [options]=useState(()=>generateOptions())
+    function generateOptions(){
+        let options=["","","",""]
+        options[Math.floor((Math.random()*4))]=props.correctAnswer
+        options.forEach((each,index)=>{
+            if(each===""){
+                options[index]=props.incorrectAnswers.pop()
+            }
+        })
+        return options
+    }
     return(
         <>
             <div className="quiz-elements">
             <p className="quiz-question">{decode(props.question)}</p>
             <div className="quiz-buttons-container">
-                <button className="quiz-options">{decode(options[0])}</button>
-                <button className="quiz-options">{decode(options[1])}</button>
-                <button className="quiz-options">{decode(options[2])}</button>
-                <button className="quiz-options">{decode(options[3])}</button>
+                {options.map((option,index)=>{
+                    return(
+                        <button key={index} className="quiz-options">{decode(option)}</button>
+                    )
+                })}
             </div>
             </div>
         </>
